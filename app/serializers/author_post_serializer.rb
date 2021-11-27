6beb5cf037d4ca99b0a46a5f94e36dcf4785ed2e
Yeison <enmanuelquiterios@gmail.com>
2021-11-27 11:40:27 -0500

    Need to review Nested routes

--- app/serializers/author_post_serializer.rb
+++ app/serializers/author_post_serializer.rb
@@ -1,8 +1,3 @@
 class AuthorPostSerializer < ActiveModel::Serializer
-  attributes :title, :content
-  def post_summary 
-    "#{self.object.title} #{self.object.content[0..39]}..."
-
-  end
-
+  attributes :id
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,5 +2,4 @@ class PostSerializer < ActiveModel::Serializer
   attributes :title, :content
   has_many :post_tags
   has_many :tags, through: :post_tags
-
 end
